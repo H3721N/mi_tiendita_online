4ed14391e70feee5e0246a89e469ea080cb9a36d
H3721N <herlingomezescobar@gmail.com>
2024-12-27 14:45:31 -0600

    Se elimina objetos innecesarios en app

---
+++ mi_tiendita_online/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginForm from "./components/login/LoginForm.jsx";
+
+function App() {
+    return (
+
+        <div>
+            <LoginForm/>
+
+        </div>
+    )
+}
+
+export default App;
