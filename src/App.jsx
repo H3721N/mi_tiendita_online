633524a83245f93db958ceb00d5e8d68c816c7a1
H3721N <herlingomezescobar@gmail.com>
2024-12-28 16:50:44 -0600

    Se corrige la vista inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
-import LoginForm from "./components/login/LoginForm.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import routes from './routes.jsx';
+
 
 function App() {
     return (
-
-        <div>
-            <LoginForm/>
-
-        </div>
-    )
+        <Router>
+            <div>
+                <Routes>
+                    {routes.map((route, index) => (
+                        <Route key={index} path={route.path} element={route.element} />
+                    ))}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
