0d1566ecbe7d63441207f1f85cfcdd7bdbadbe00
H3721N <herlingomezescobar@gmail.com>
2024-12-28 16:53:54 -0600

    Se agrega ReactDom para el cambio de vista segun la ruta

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import {BrowserRouter} from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import App from "./App.jsx";
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
+ReactDOM.render(
+    <React.StrictMode>
         <App />
-    </BrowserRouter>
-)
+    </React.StrictMode>,
+    document.getElementById('root')
+);
