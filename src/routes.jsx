9aa5d14b0f4281124c517bfed151cb60d259a471
H3721N <herlingomezescobar@gmail.com>
2024-12-29 00:49:38 -0600

    Se agrega la ruta para utilizar register

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import { HomePage } from './pages/homePage/HomePage.jsx';
 import { LoginForm } from './components/login/LoginForm.jsx';
+import { RegisterForm} from "./components/RegisterForm/RegisterForm.jsx";
 
 const routes = [
     { path: '/auth', element: <LoginForm /> },
     { path: '/home', element: <HomePage /> },
+    { path: '/register', element: <RegisterForm /> }
 ]
 
 export default routes;
