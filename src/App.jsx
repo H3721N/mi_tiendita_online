54c43d6ddcbb2959d6d4946c4ed8ee84c5a332d3
H3721N <herlingomezescobar@gmail.com>
2024-12-31 15:01:30 -0600

    Se agrega el nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './routes.jsx';
 import ButtonAppBar from "./components/navbar/Nabvar.jsx";
 import React from "react";
-
+import { CartProvider } from "./context/cart.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
 
 function App() {
     return (
         <Router>
             <div>
-                <Routes>
-                    {routes.map((route, index) => (
-                        <Route key={index} path={route.path} element={route.element} />
-                    ))}
-                </Routes>
+                <CartProvider>
+                    <Routes>
+                        {routes.map((route, index) => (
+                            <Route key={index} path={route.path} element={route.element} />
+                        ))}
+                    </Routes>
+                    <Footer />
+                </CartProvider>
             </div>
         </Router>
     );
