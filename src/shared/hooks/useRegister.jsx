77090f5f5cc3054f7749e17dfea790f50298e0e7
H3721N <herlingomezescobar@gmail.com>
2024-12-30 23:48:15 -0600

    Se corrige userRegister

---
+++ src/shared/hooks/useRegister.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { registerUser as registerRequest } from "../../services/authService.jsx";
+import toast from "react-hot-toast";
+
+export const useRegister = () => {
+    const [isRegister, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const registerUser = async (nombre, telefono, email, fechaNacimiento,
+                                nombreComercial, razonSocial, direccionEntrega,
+                                password, confirmPassword) => {
+        setIsLoading(true);
+
+        const response = await registerRequest({
+            nombre, telefono, email, fechaNacimiento,
+            nombreComercial, razonSocial, direccionEntrega,
+            password, confirmPassword
+        });
+
+        setIsLoading(false);
+
+        if (response.error) {
+            toast.error("Error al registrar usuario");
+            return;
+        }
+
+        navigate("/auth", { state: { email, password } });
+    };
+
+    return { registerUser, isRegister };
+}
