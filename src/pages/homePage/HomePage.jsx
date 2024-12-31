cf33ef7e199cf0f87ae5c1e00bfabb1037e1dcf5
H3721N <herlingomezescobar@gmail.com>
2024-12-30 23:49:51 -0600

    Se agrega la lista de productos a homepage

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import './HomePage.css';
+import ButtonAppBar from "../../components/navbar/Nabvar";
+import MediaCard from "../../components/productCard/ProductCard.jsx";
+import { useGetProduct } from "../../shared/hooks/useGetProduct.jsx";
 
 export const HomePage = () => {
+    const [products, loading, error] = useGetProduct();
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
     return(
         <div>
-            <h1>Home Page</h1>
+            <ButtonAppBar />
+            <div className='home-wrapper'>
+                <h1>Home Page</h1>
+                {Array.isArray(products?.data) ? (
+                    products.data.map(product => (
+                        console.log('product', product),
+                            <MediaCard key={product.id} product={product} />
+                    ))
+                ) : (
+                    <p>No products available</p>
+                )}
+            </div>
         </div>
     );
 }
