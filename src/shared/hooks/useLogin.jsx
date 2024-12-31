2a8f2bbfebccbedca425bf2d61f9cb6d5b1d794b
H3721N <herlingomezescobar@gmail.com>
2024-12-30 23:47:47 -0600

    Se corrige useLogin

--- src/shared/hooks/useLogin.jsx
+++ src/shared/hooks/useLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { login as loginRequest } from "../../services/api.jsx";
+import { login as loginRequest } from "../../services/authService.jsx";
 import toast from 'react-hot-toast';
 
 export const useLogin = () => {
