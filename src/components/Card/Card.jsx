1cd70693e8fddd2f464fb770e00320d4073c904a
H3721N <herlingomezescobar@gmail.com>
2024-12-30 23:53:57 -0600

    Se crea el componente de cart y su estilo

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,33 @@
+import { FaCartPlus } from "react-icons/fa";
+import { BsCartXFill } from "react-icons/bs";
+import {useId} from "react";
+
+export function Cart () {
+    const cartCheckboxId = useId()
+    return (
+        <>
+            <label className='cart-button' htmlFor={cartCheckboxId}>
+                <FaCartPlus />
+            </label>
+            <input id={cartCheckboxId} type='checkbox' hidden/>
+
+            <aside className='cart'>
+                <ul>
+                    <li>
+                        <img src='https://via.placeholder.com/150' alt='product'/>
+                        <div>
+                            <h3>Product name</h3>
+                            <p>$100.00</p>
+                        </div>
+                        <footer>
+                            Qty: 1
+                        </footer>
+                    </li>
+                </ul>
+                <button>
+                    <BsCartXFill />
+                </button>
+            </aside>
+        </>
+    );
+}
