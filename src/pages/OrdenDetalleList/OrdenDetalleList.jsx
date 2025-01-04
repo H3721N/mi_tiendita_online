f082cc8a0088de03306539d9f755c93575251442
H3721N <herlingomezescobar@gmail.com>
2025-01-04 00:03:55 -0600

    Listas de detalles y ordenes

--- src/pages/OrdenDetalleList/OrdenDetalleList.jsx
+++ src/pages/OrdenDetalleList/OrdenDetalleList.jsx
@@ -1,61 +1,25 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import { useGetOrden } from '../../shared/hooks/useGetOrden';
-import Typography from "@mui/material/Typography";
-import Button from '@mui/material/Button';
+import React from 'react';
+import { Grid, Paper, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, TablePagination, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { useOrdenById } from '../../shared/hooks/useordenDetalleById';
-import {Grid} from "@mui/joy";
 
-const getColumnsByRole = (role) => {
-    const commonColumns = [
-        { id: 'id', label: 'ID de Orden' },
-        { id: 'direccion', label: 'Dirección' },
-        { id: 'telefono', label: 'Teléfono' },
-        { id: 'fechaEntrega', label: 'Fecha de Entrega' },
-        { id: 'total', label: 'Total de la Orden', align: 'right', format: (value) => value.toFixed(2) },
-    ];
+const columns = [
+    { id: 'id', label: 'ID' },
+    { id: 'idOrden', label: 'ID Orden' },
+    { id: 'idProducto', label: 'ID Producto' },
+    { id: 'cantidad', label: 'Cantidad' },
+    { id: 'precio', label: 'Precio' },
+    { id: 'subtotal', label: 'Subtotal' },
+    { id: 'Producto', label: 'Producto' },
+    { id: 'negar', label: 'Botón' }
+];
 
-    if (role === 'Admin') {
-        return [
-            { id: 'id', label: 'ID de Orden' },
-            { id: 'nombre', label: 'Nombre Completo' },
-            { id: 'email', label: 'Correo Electrónico' },
-            ...commonColumns.slice(1),
-            { id: 'negar', label: 'Botón' }
-        ];
-    }
-
-    return commonColumns;
-};
-
-const decodeJWT = (token) => {
-    if (!token) {
-        return null;
-    }
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-};
-
-const decodedToken = decodeJWT(localStorage.getItem('token'));
-const columns = getColumnsByRole(decodedToken.userRol);
-
-export default function OrdenList() {
+export default function OrdenDetalleList() {
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(105);
-    const [ordenes, loading, error] = useGetOrden(page, rowsPerPage);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const [selectedOrderId, setSelectedOrderId] = React.useState(null);
-    const { orderDetail, isLoading: detailLoading, error: detailError } = useOrdenById(selectedOrderId);
+    const { orderDetail, isLoading, error } = useOrdenById(selectedOrderId);
+    const navigate = useNavigate();
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -67,23 +31,22 @@ export default function OrdenList() {
     };
 
     const handleButtonClick = (id) => {
-        setSelectedOrderId(id);
+        navigate(`/orderDetail/${id}`);
     };
 
-
-    if (loading || detailLoading) {
+    if (isLoading) {
         return <div>Loading...</div>;
     }
 
-    if (error || detailError) {
-        return <div>Error: {error ? error.message : detailError.message}</div>;
+    if (error) {
+        return <div>Error: {error}</div>;
     }
 
     return (
         <Grid container spacing={2} className='row'
-              justifyContent="center" // Centrar horizontalmente
-              alignItems="flex-start" // Alineación vertical al inicio
-              sx={{ minHeight: '100vh', padding: 2 }} // Altura mínima y relleno
+              justifyContent="center"
+              alignItems="flex-start"
+              sx={{ minHeight: '100vh', padding: 2 }}
         >
             <Grid xs={12} sm={12} md={12} lg={12}>
                 <Paper sx={{width: '100%', overflow: 'hidden',
@@ -113,13 +76,13 @@ export default function OrdenList() {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {Array.isArray(ordenes) && ordenes
+                                {Array.isArray(orderDetail) && orderDetail
                                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                                     .map((row) => {
                                         return (
                                             <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                                                 {columns.map((column) => {
-                                                    const value = row[column.id];
+                                                    const value = column.id === 'Producto' ? row.Producto.nombre : row[column.id];
                                                     return (
                                                         <TableCell key={column.id} align={column.align}>
                                                             {column.id === 'negar' ? (
@@ -150,7 +113,7 @@ export default function OrdenList() {
                         showLastButton={true}
                         rowsPerPageOptions={[5, 10, 25, 100]}
                         component="div"
-                        count={Array.isArray(ordenes) ? ordenes.length : 0}
+                        count={Array.isArray(orderDetail) ? orderDetail.length : 0}
                         rowsPerPage={rowsPerPage}
                         page={page}
                         onPageChange={handleChangePage}
@@ -158,7 +121,6 @@ export default function OrdenList() {
                     />
                 </Paper>
             </Grid>
-
         </Grid>
     );
 }
