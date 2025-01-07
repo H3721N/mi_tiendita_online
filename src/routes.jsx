6772ed3ebf50fe6f97c7411d363eec51349ffd7f
H3721N <herlingomezescobar@gmail.com>
2025-01-06 18:53:21 -0600

    Se agrega la proteccion de rutas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,20 @@
-import { HomePage } from './pages/homePage/HomePage.jsx';
+import { HomePage } from './pages/HomePage/HomePage.jsx';
 import { LoginForm } from './components/login/LoginForm.jsx';
-import { RegisterForm} from "./components/RegisterForm/RegisterForm.jsx";
+import { RegisterForm } from "./components/RegisterForm/RegisterForm.jsx";
+import OrdenList from "./pages/OrdenList/OrdenList.jsx";
+import OrderDetail from "./pages/OrdenDetalleList/OrdenDetalleList.jsx";
+import ControllerProduct from "./pages/ContolerProduct/controllerProduct.jsx";
+import ControlerUser from "./pages/ControlerUser/ControlerUser.jsx";
+import { ProtectedRoute, ProtectedFunctionOperador } from "./components/ProtectedRoute/ProtectedRoute.jsx";
 
 const routes = [
-    { path: '/auth', element: <LoginForm /> },
-    { path: '/home', element: <HomePage /> },
-    { path: '/register', element: <RegisterForm /> }
-]
+    { path: '/', element: <LoginForm /> },
+    { path: '/home', element: <ProtectedRoute><HomePage /></ProtectedRoute> },
+    { path: '/register', element: <RegisterForm /> },
+    { path: '/orden', element: <ProtectedRoute><OrdenList /></ProtectedRoute> },
+    { path: '/orderDetail/:id', element: <ProtectedRoute><OrderDetail /></ProtectedRoute> },
+    { path: '/product', element: <ProtectedRoute><ProtectedFunctionOperador><ControllerProduct /></ProtectedFunctionOperador></ProtectedRoute> },
+    { path: '/users', element: <ProtectedRoute><ProtectedFunctionOperador><ControlerUser /></ProtectedFunctionOperador></ProtectedRoute> },
+];
 
 export default routes;
