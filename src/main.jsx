c39aadf4d770a1aac72d780e6af1f2cd532ae6ea
H3721N <herlingomezescobar@gmail.com>
2025-01-06 18:53:03 -0600

    Se elimina objeto innecesario

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import { CartProvider } from './context/cart.jsx';
 import './index.css';
 import ButtonAppBar from "./components/navbar/Nabvar.jsx";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <CartProvider>
             <App />
